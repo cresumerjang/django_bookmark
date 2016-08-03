d29dbcca37c8dbb5d19306639b4441a4f1e6209e
JANG JAE WON <cresumerjang@gmail.com>
2016-08-03 22:41:43 +0900

    템플릿 상속 및 모델수정

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import ListView
 from bookmark.views import *
 
 urlpatterns = [
-    url(r'^$',  BookmarkLV.as_view(),name='index'),
+    url(r'^$', BookmarkLV.as_view(), name='list'),
+    url(r'^(?P<pk>[0-9]+)/$',  BookmarkDV.as_view(), name='detail'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -4,9 +4,11 @@ from bookmark.models import Bookmark
 
 class BookmarkLV(ListView):
     model = Bookmark
-    template_name = 'bookmark_list.html'
+    context_object_name = 'bookmark_list'
+    template_name = 'bookmark/bookmark_list.html'
 
 class BookmarkDV(DetailView):
     model = Bookmark
-    template_name = 'bookmark_detail.html'
+    context_object_name = 'bookmark_detail'
+    template_name = 'bookmark/bookmark_detail.html'
 
