3d63b8b2e4f4b041295182f727421fef0d1df7e7
JANG JAE WON <cresumerjang@gmail.com>
2016-08-03 21:17:27 +0900

    django debug toolbar설치

--- superjang/settings.py
+++ superjang/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.debug',
+    'debug_toolbar', # pip3 install django-debug-toolbar
 ]
 
 MIDDLEWARE_CLASSES = [
