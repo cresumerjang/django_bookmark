bf6d05b5bbda45b2176f03ab271a3b545c07053c
JANG JAE WON <cresumerjang@gmail.com>
2016-08-03 21:15:02 +0900

    startproject

---
+++ superjang/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for superjang project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superjang.settings")
+
+application = get_wsgi_application()
